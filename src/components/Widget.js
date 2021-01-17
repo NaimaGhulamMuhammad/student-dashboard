dda5341e119a3e9aefd4be8d22e2f21267c00fc5
Naima Ghulam Muhammad <naimaghm92@gmail.com>
2021-01-17 11:35:15 +0500

    added images

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -3,10 +3,10 @@ import {Card, CardMedia,CardActions, Button} from "@material-ui/core"
 import avatar from "../images/avatar.png"
 
 function Widget(props) {
-    const {text, link} = props
+    const {text, image, link} = props
     return (
         <Card >
-           <CardMedia src={avatar}  height="220px" component="img" alt={text}  />
+           <CardMedia src={image}  height="220px" component="img" alt={text}  />
            <CardActions className="justify-content-center mt-3">
                <Button variant="contained" className=" bgColor text-light m-2" style={{width:"100%"}} onClick={link}>{text}</Button>
            </CardActions>
